77b574b9cf8582408d63ce15db948ace8a9a93cc
Sebastian Alsina <alsinas@me.com>
2022-01-25 22:02:58 -0500

    Added Smexy Gradient

--- src/pages/singularity/index.tsx
+++ src/pages/singularity/index.tsx
@@ -1,13 +1,21 @@
-import { createContext } from 'react'
-import NavBar from '../../components/Singularity/NavBar'
+import { useEffect } from 'react'
 import { SingularityAppWrapper } from '../../components/Singularity/SingularityAppWrapper'
 import Swapper from '../../components/Singularity/Swapper'
+import { Gradient } from '../../lib/gradient'
 
 export default function SingularityIndexPage() {
+    useEffect(() => {
+        const gradient = new Gradient()
+        gradient.initGradient('#gradient-canvas')
+    }, [])
+
     return (
         <>
             <SingularityAppWrapper>
-                <div className="w-full h-full bg-center bg-cover p-6 py-24" style={{ backgroundImage: `url("/img/singularity-background-2.png")` }}>
+                <div className="w-full h-full bg-center bg-cover p-6 py-24">
+                    <div className="absolute inset-0">
+                        <canvas id="gradient-canvas" data-transition-in></canvas>
+                    </div>
                     <div className="bg-black absolute inset-0 bg-opacity-50"></div>
                     <Swapper />
                 </div>
