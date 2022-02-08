f37275e556bdb55f14db4de088fdfa8cb037c2c5
Sebastian Alsina <alsinas@me.com>
2022-02-08 02:12:14 -0500

    Remove Market name from Header

--- src/pages/creditum/index.tsx
+++ src/pages/creditum/index.tsx
@@ -92,10 +92,10 @@ export default function Creditum() {
                             <>
                                 <div className="space-y-8">
                                     <div className="space-y-4">
-                                        <div className="flex items-center space-x-4">
+                                        {/* <div className="flex items-center space-x-4">
                                             <img className="w-16 h-16" src={`/img/tokens/${selectedMarket.asset}`} alt="" />
                                             <p className="text-2xl font-medium">{selectedMarket.symbol} Market</p>
-                                        </div>
+                                        </div> */}
                                         {/* <div className="bg-yellow-500 text-neutral-900 p-4">
                                         <p className="font-medium text-sm">Your Position</p>
                                         <p>123</p>
