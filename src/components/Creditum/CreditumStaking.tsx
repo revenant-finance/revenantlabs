5a8c78b1aefe465d3cc2b95b2ae46ed07b6c26b2
xam-darnold <82432557+xam-darnold@users.noreply.github.com>
2022-02-19 23:44:19 -0600

    add veCREDIT balance

--- src/components/Creditum/CreditumStaking.tsx
+++ src/components/Creditum/CreditumStaking.tsx
@@ -121,6 +121,7 @@ export default function CreditumStaking() {
                         <DataPoint title="Credit Balance" value={`${formatter(veCreditData.tokenBal)}`} />
                         <DataPoint title="Total Locked" value={`${formatter(veCreditData.veCreditTotalSupply)}`} />
                         <DataPoint title="User Amount Locked" value={`${formatter(veCreditData.creditLocked)} CREDIT`} />
+                        <DataPoint title="User veCREDIT Balance" value={`${formatter(veCreditData.veCreditBal)} veCREDIT`} />
                         <DataPoint title="Time Until Unlock" value={`${formatter((veCreditData.lockEnd - +new Date() / 1000) / (60 * 60) > 0 ? (veCreditData.lockEnd - +new Date() / 1000) / (60 * 60) : 0)} hours`} />
                     </div>
 
