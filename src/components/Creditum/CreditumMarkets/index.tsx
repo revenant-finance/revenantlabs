a4fb01722ed098f1460028b0d309010ae05bd630
Sebastian Alsina <alsinas@me.com>
2022-03-23 16:04:28 -0400

    Static Data

--- src/components/Creditum/CreditumMarkets/index.tsx
+++ src/components/Creditum/CreditumMarkets/index.tsx
@@ -17,16 +17,15 @@ export default function CreditumMarkets() {
 
     return (
         <div className="w-full p-6 mx-auto space-y-12 max-w-7xl">
+            <div className="col-span-2">
+                <MarketTicker />
+            </div>
+
             <InfoBanner
                 header="Markets"
                 title="Stabilize your fortunes by minting cUSD."
                 subtitle="Users can deposit crypto assets as collateral to mint cUSD, a stablecoin pegged to USD. Interest rates are fixed so no need to worry about market fluctuation. cUSD and CREDIT can be used for various farming rewards."
             />
-
-            <div className="col-span-2">
-                <MarketTicker />
-            </div>
-
             <div className="grid grid-cols-1 gap-12 md:grid-cols-2">
                 <div className="space-y-8">
                     {!markets && <LoadingBanner title="Markets are loading..." />}
--- src/components/MarketTicker.tsx
+++ src/components/MarketTicker.tsx
@@ -28,38 +28,68 @@ export default function MarketTicker() {
     const tvl = creditumTvl + farmTvl + xTokenTvl + veTokenTvl
 
     return (
-        <div className="p-2 bg-opacity-50 border-2 shadow-2xl bg-neutral-800 border-neutral-800 rounded-2xl">
-            {!tvl && (
-                <p className="font-medium text-center opacity-50">
-                    <ReactTyped strings={['Loading...']} loop />
-                </p>
-            )}
-            {!!tvl && (
-                <Ticker>
-                    {({ index }) => (
-                        <>
-                            <div className="flex items-center mr-4 space-x-4">
-                                <MarketTickerItem
-                                    title="CREDIT Market Cap"
-                                    value={`$${formatter(govTokenData?.marketCap)}`}
-                                />
-                                <MarketTickerItem
-                                    title="CREDIT Circulating Supply"
-                                    value={`${formatter(govTokenData?.circSupply)}`}
-                                />
-                                <MarketTickerItem
-                                    title="CREDIT Price"
-                                    value={`$${formatter(govTokenData?.tokenPrice)}`}
-                                />
-                                <MarketTickerItem
-                                    title="Creditum Total Value Locked (TVL)"
-                                    value={`$${formatter(tvl)}`}
-                                />
-                            </div>
-                        </>
+        <>
+            <div className="space-y-6">
+                <div className="flex justify-end items-end gap-6">
+                    <div className="space-y-1 flex-1">
+                        <p className="text-2xl lg:text-4xl font-extended">
+                            <span className="text-transparent bg-clip-text bg-gradient-to-br from-white to-blue-300">
+                                ${formatter(tvl)}
+                            </span>
+                        </p>
+                        <p className="text-xs lg:text-lg opacity-75">Total Value Locked (TVL)</p>
+                    </div>
+                    <div className="space-y-1">
+                        <p className="text-2xl lg:text-4xl font-extended">
+                            <span className="text-transparent bg-clip-text bg-gradient-to-br from-white to-blue-300">
+                                ${formatter(govTokenData?.tokenPrice)}
+                            </span>
+                        </p>
+                        <p className="text-xs lg:text-lg opacity-75">CREDIT Price</p>
+                    </div>
+                    <div className="space-y-1">
+                        <p className="text-2xl lg:text-4xl font-extended">
+                            <span className="text-transparent bg-clip-text bg-gradient-to-br from-white to-blue-300">
+                                {formatter(govTokenData?.circSupply)}
+                            </span>
+                        </p>
+                        <p className="text-xs lg:text-lg opacity-75">CREDIT Supply</p>
+                    </div>
+                </div>
+                <div className="p-2 bg-opacity-50 border-2 shadow-2xl bg-neutral-800 border-neutral-800 rounded-2xl">
+                    {!tvl && (
+                        <p className="font-medium text-center opacity-50">
+                            <ReactTyped strings={['Loading...']} loop />
+                        </p>
                     )}
-                </Ticker>
-            )}
-        </div>
+                    {!!tvl && (
+                        <Ticker>
+                            {({ index }) => (
+                                <>
+                                    <div className="flex items-center mr-4 space-x-4">
+                                        <MarketTickerItem
+                                            title="CREDIT Market Cap"
+                                            value={`$${formatter(govTokenData?.marketCap)}`}
+                                        />
+                                        <MarketTickerItem
+                                            title="CREDIT Circulating Supply"
+                                            value={`${formatter(govTokenData?.circSupply)}`}
+                                        />
+                                        <MarketTickerItem
+                                            title="CREDIT Price"
+                                            value={`$${formatter(govTokenData?.tokenPrice)}`}
+                                        />
+                                        <MarketTickerItem
+                                            title="Creditum Total Value Locked (TVL)"
+                                            value={`$${formatter(tvl)}`}
+                                        />
+                                    </div>
+                                </>
+                            )}
+                        </Ticker>
+                    )}
+                </div>
+            </div>
+        </>
     )
 }
