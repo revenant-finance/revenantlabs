45cc24e81f51498a6603057c2a07da20b3a41c49
Sebastian Alsina <alsinas@me.com>
2022-03-25 17:41:11 -0400

    Added Withdraw

--- src/components/Singularity/SingularityLiquidity/SingularityLiquidityModal.tsx
+++ src/components/Singularity/SingularityLiquidity/SingularityLiquidityModal.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import useSingularity from '../../../hooks/useSingularity'
 import Button from '../../Button'
 import Input from '../../Input'
@@ -6,12 +7,18 @@ import Modal from '../../Modal'
 export default function SingularityLiquidityModal() {
     const { lpInput, setLpInput, selectedLp, setSelectedLp } = useSingularity()
 
+    const [isWithdraw, setIsWithdraw] = useState(false)
+
     return (
         <>
             <Modal visible={selectedLp ? true : false} onClose={() => setSelectedLp(null)}>
                 <div className="space-y-4">
+                    <Button onClick={() => setIsWithdraw((_) => !_)} className="bg-blue-500">
+                        {isWithdraw ? 'Withdraw' : 'Deposit'}
+                    </Button>
+
                     <div className="space-y-2">
-                        <p>confirm deposit</p>
+                        <p>confirm {isWithdraw ? 'withdraw' : 'deposit'}</p>
                         <Input
                             type="number"
                             value={lpInput}
@@ -22,21 +29,36 @@ export default function SingularityLiquidityModal() {
                             <p>data</p>
                         </div>
                     </div>
-                    <div>
-                        <p>Deposit: {lpInput}</p>
-                        <p>tokenPrice: {selectedLp?.tokenPrice}</p>
-                        <p>Fees: {'?????????'}</p>
-                    </div>
-                    <div>
-                        <p>Ur Deposit: {lpInput}</p>
-                        <p>
-                            Pool Share: {'?????????'} {'=>'} {'?????????'}
-                        </p>
-                    </div>
+
+                    {!isWithdraw && (
+                        <div>
+                            <p>Deposit: {lpInput}</p>
+                            <p>tokenPrice: {selectedLp?.tokenPrice}</p>
+                            <p>Fees: {'?????????'}</p>
+
+                            <p>
+                                Ur Deposits:{'?????????'} {'=>'} {'?????????'}
+                            </p>
+                        </div>
+                    )}
+
+                    {isWithdraw && (
+                        <div>
+                            <p>Price Oracle: {selectedLp?.tokenPrice}</p>
+                            <p>Amount Withdrawn: {lpInput}</p>
+                            <p>Fees: {'?????????'}</p>
+
+                            <p>
+                                Ur Deposits:{'?????????'} {'=>'} {'?????????'}
+                            </p>
+                        </div>
+                    )}
 
                     <div className="flex space-x-6">
                         <Button>Cancel</Button>
-                        <Button className="bg-blue-500">Approve</Button>
+                        <Button className="bg-blue-500">
+                            {isWithdraw ? 'Withdraw' : 'Deposit'}
+                        </Button>
                     </div>
                 </div>
             </Modal>
