ab1206c0c7b237b6ac7d4d4ea8347d50b528e440
xam-darnold <82432557+xam-darnold@users.noreply.github.com>
2022-05-02 18:58:33 -0500

    Revert "redeployment of stable pools"
    This reverts commit 093d50fe190ff197f9e00b3ce78c9f36fd738af5.

--- src/Singularity/SingularityLiquidity/SingularityLiquidityModal.tsx
+++ src/Singularity/SingularityLiquidity/SingularityLiquidityModal.tsx
@@ -26,7 +26,7 @@ export default function SingularityLiquidityModal() {
         slippageTolerance,
         setSlippageTolerance,
         withdrawFee,
-        depositFee,
+        depositReward,
         withdrawLp,
         depositLp,
         mintTestToken,
@@ -71,8 +71,8 @@ export default function SingularityLiquidityModal() {
                                 />
 
                                 <DataPoint
-                                    title={`Deposit Fee`}
-                                    value={`${formatter(depositFee)} ${selectedLp?.symbol}`}
+                                    title={`Reward`}
+                                    value={`${formatter(depositReward)} ${selectedLp?.symbol}`}
                                 />
                             </>
                         )}
@@ -221,8 +221,8 @@ export default function SingularityLiquidityModal() {
 
                         {!isWithdraw && (
                             <DataPoint
-                                title={'Deposit Fee'}
-                                value={`${depositFee} ${selectedLp?.symbol}`}
+                                title={'Deposit Reward'}
+                                value={`${depositReward} ${selectedLp?.symbol}`}
                             />
                         )}
 
